fd51eb04fe67c09d82a97d9293b957308dfac3e7
deluwarhoisn <babuhossen301@gmail.com>
2025-09-03 23:21:52 +0600

    Enhance vocabulary section and lesson loading functionality in index.html and index.js

--- script/index.js
+++ script/index.js
@@ -1,22 +1,40 @@
 const loadLessons = ()=>{
     fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLesson(json.data));
+};
+const loadLevelWord =(id)=>{
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
     .then(res=> res.json())
-    .then((json)=> displayLesson(json.data));
+    .then(data=> displayLevelWords(data.data))
 }
 
-const displayLesson=(lessons)=>{
+const displayLevelWords = (words)=> {
+const wordContainer = document.getElementById('word-container')
+wordContainer.innerHTML = "";
+words.forEach(word =>{
+    const card=document.createElement('div');
+    card.innerHTML=`
+    <p>cat</p>
+    `;
+    wordContainer.append(card);
+})
+}
+
+const displayLesson = (lessons)=>{
 // 1. get the container & empty
-const levelContainer = document.getElementById("level-container");
-levelContainer = "";
+const levelContainer = document.getElementById('level-container');
+levelContainer.innerHTML = "";
 // 2. get into ever lessons
 for(let lesson of lessons){
 //    3. create Element
-const btnDiv = document.createElement("div");
+const btnDiv = document.createElement('div');
 btnDiv.innerHTML=`
- <button class="btn btn-outline btn-primary"> <i class="fa-solid fa-book-open-reader"></i> Lesson - ${lesson.level_no}</button>
+ <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"> <i class="fa-solid fa-book-open-reader"></i> Lesson - ${lesson.level_no}</button>
 `;
 //      4. append intro container
-levelContainer.append(btnDiv);
+levelContainer.append(btnDiv)
 }
 //   
 
